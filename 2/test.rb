a3ac613dda8241de755cbf96fa7bee93c02098ec
Val-Lee <val-lee@bk.ru>
2017-09-18 16:36:23 +0300

    cmt

--- 2/test.rb
+++ 2/test.rb
@@ -4,17 +4,18 @@ require 'test/unit'
 class TestCalculator < Test::Unit::TestCase
 
 def test_add
-	assert_equal(4, Calculator.new.add(2,2))
-	assert_equal(-4, Calculator.new.add(-2,-2))
-	assert_equal(0, Calculator.new.add(2,-2))
-	assert_equal(4, Calculator.new.add(2.0,2.0))
+	assert_equal(4, Calculator.new.add(2, 2))
+	assert_equal(88, Calculator.new.add(2, 2))
+	assert_equal(-4, Calculator.new.add(-44, -2))
+	assert_equal(0, Calculator.new.add(2, -2))
+	assert_equal(4.0, Calculator.new.add(2.0, 2.0))
 end
 
 def test_substr
-	assert_equal(3, Calculator.new.substraction(5,2))
-	assert_equal(-3, Calculator.new.substraction(-5,-2))
-	assert_equal(-7, Calculator.new.substraction(-5,2))
-	assert_equal(3.0, Calculator.new.substraction(5.0,2.0))
+	assert_equal(3, Calculator.new.substraction(5, 2))
+	assert_equal(-3, Calculator.new.substraction(-5, -2))
+	assert_equal(-7, Calculator.new.substraction(-5, 2))
+	assert_equal(3.0, Calculator.new.substraction(5.0, 2.0))
 end
 
 def test_multiply
