b392c05e19957ea511ba4ac5bee9b0df87926369
Valentin Lee <vallee.troll@gmail.com>
2018-01-03 15:24:20 +0300

    Add cases

--- 4/spec/airlines_spec.rb
+++ 4/spec/airlines_spec.rb
@@ -1,25 +1,82 @@
-require "spec_helper"
+require 'spec_helper'
 require 'selenium-webdriver'
 
 describe 'singaporeair' do
 
   before(:each) do
-    @driver = Selenium::WebDriver.for :chrome
-    @base_url = 'http://www.singaporeair.com/'
-    @accept_next_alert = true
-    @driver.manage.timeouts.implicit_wait = 30
-    @driver.get(@base_url)
+    init
   end
 
   after(:each) do
+    sleep 5
     @driver.quit
   end
 
   it 'case 1' do
-    @driver.find_element(:id, 'city-1').clear
-    @driver.find_element(:id, 'city-1').send_keys 'Singapore - SIN'
-    @driver.find_element(:id, 'city-1').click
-    @driver.find_element(:id, 'city-travel-input-2').click
+    direction('Singapore - SIN', 'Lisbon - LIS')
+    set_dates(DEPART_DATE, RETURN_DATE)
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[2]/h2").text
+    expect(list_of_fligths).to eq('Select flights')
   end
 
+  it 'case 2' do
+    direction('Singapore - SIN', 'Milan - MXP')
+    set_dates(DEPART_DATE, DEPART_DATE)
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[1]/h2").text
+    expect(list_of_fligths).to eq('Select alternative date(s)')
+  end
+
+  it 'case 3' do
+    set_class('business')
+    set_type('one-way')
+    direction('Singapore - SIN', 'Moscow - DME')
+    @driver.find_element(:id, 'city-travel-start-day-2').clear
+    @driver.find_element(:id, 'city-travel-start-day-2').send_keys DEPART_DATE
+    # clear_popouts
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[1]/h2").text
+    expect(list_of_fligths).to eq('Select alternative date(s)')
+  end
+
+  it 'case 4' do
+    direction('Singapore - SIN', 'Lisbon - LIS')
+    set_dates
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[2]/h2").text
+    expect(list_of_fligths).to eq('Select flights')
+  end
+
+  it 'case 5' do
+    direction('Singapore - SIN', 'Lisbon - LIS')
+    set_dates
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[2]/h2").text
+    expect(list_of_fligths).to eq('Select flights')
+  end
+
+  it 'case 6' do
+    direction('Singapore - SIN', 'Lisbon - LIS')
+    set_dates
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[2]/h2").text
+    expect(list_of_fligths).to eq('Select flights')
+  end
+
+  it 'case 7' do
+    direction('Singapore - SIN', 'Lisbon - LIS')
+    set_dates
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[2]/h2").text
+    expect(list_of_fligths).to eq('Select flights')
+  end
+
+  it 'case 7' do
+    direction('Singapore - SIN', 'Lisbon - LIS')
+    set_dates
+    search
+    list_of_fligths = @driver.find_element(:xpath, "//*[@id='main-inner']/div[2]/h2").text
+    expect(list_of_fligths).to eq('Select flights')
+  end
 end
